917fcd04f14e8ec9d1e7a1b6e635d71dcd9e5516
InnovativeGlazing <innovativeglazing@proton.me>
2025-01-26 14:13:28 -0500

    removed video

--- src/sections/Tokenomics.vue
+++ src/sections/Tokenomics.vue
@@ -2,12 +2,12 @@
   <div class="flex flex-col items-center gap-10" id="tokenomics">
     <div class="text-3xl font-bold">Tokenomics</div>
     <div class="md:w-3/4 border rounded-lg px-8 py-4">
-      <video
-          src="/assets/launch.mp4"
-          width="100%"
-          height="100%"
-          controls
-      ></video>
+<!--      <video-->
+<!--          src="/assets/launch.mp4"-->
+<!--          width="100%"-->
+<!--          height="100%"-->
+<!--          controls-->
+<!--      ></video>-->
     </div>
   </div>
 </template>
