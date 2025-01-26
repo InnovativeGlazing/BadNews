251ef7cba105e873cb593a913da16c495aff1062
InnovativeGlazing <innovativeglazing@proton.me>
2025-01-26 14:21:36 -0500

    added video back

--- src/sections/Tokenomics.vue
+++ src/sections/Tokenomics.vue
@@ -1,13 +1,12 @@
 <template>
   <div class="flex flex-col items-center gap-10" id="tokenomics">
-    <div class="text-3xl font-bold">Tokenomics</div>
     <div class="md:w-3/4 border rounded-lg px-8 py-4">
-<!--      <video-->
-<!--          src="/assets/launch.mp4"-->
-<!--          width="100%"-->
-<!--          height="100%"-->
-<!--          controls-->
-<!--      ></video>-->
+      <video
+          src="/assets/launch.mp4"
+          width="100%"
+          height="100%"
+          controls
+      ></video>
     </div>
   </div>
 </template>
